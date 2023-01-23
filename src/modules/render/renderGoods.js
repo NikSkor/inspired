9f282ae222d12252e6759e6255bf5311eb9bf29d
NikSkor <mr-zerx@yandex.ru>
2023-01-23 21:04:55 +0500

    product colors done

--- src/modules/render/renderGoods.js
+++ src/modules/render/renderGoods.js
@@ -1,4 +1,4 @@
-import { API_URL } from "../const";
+import { API_URL, DATA } from "../const";
 import { createElement } from "../createElement";
 import { getData } from "../getData";
 
@@ -32,39 +32,65 @@ export const renderGoods = async (title, params) => {
   const listCard = products.map((product) => {
     const li = createElement('li', {
       className: 'goods__item',
-      innerHTML: `
-        <article class="product">
-              <a href="#/goods/${product.id}" class="product__link">
-                <img
-                  src="${API_URL}/${product.pic}"
-                  alt="${product.title}"
-                  class="product__image"
-                />
-                <h3 class="product__title">
-                  ${product.title}
-                </h3>
-              </a>
-              <div class="product__row">
-                <p class="product__price">руб ${product.price}</p>
-                <button
-                  class="product__btn-favorite"
-                  data-id="${product.id}"
-                  aria-label="Добавить в избранное"
-                ></button>
-              </div>
-            </article>
-      `,
     });
+
+    const article = createElement('article', {
+      className: 'product',
+      innerHTML: `
+        <a href="#/goods/${product.id}" class="product__link">
+          <img
+            src="${API_URL}/${product.pic}"
+            alt="${product.title}"
+            class="product__image"
+          />
+          <h3 class="product__title">
+            ${product.title}
+          </h3>
+        </a>
+        <div class="product__row">
+          <p class="product__price">руб ${product.price}</p>
+          <button
+            class="product__btn-favorite"
+            data-id="${product.id}"
+            aria-label="Добавить в избранное"
+          ></button>
+        </div>
+      `,
+    }, {
+      parent: li,
+    })
+
+    const ulColor = createElement(
+      'ul',
+      {
+        className: 'product__color-list',
+      },
+      {
+        parent: article,
+        appends: product.colors.map((colorId, i) => {
+          const color = DATA.colors.find(item => item.id == colorId);
+          return createElement('li', {
+            className: `color color_${color.title} ${i ? '' : 'color_check'}`,
+          });
+        })
+      }
+    );
+
+    
 
     return li;
   });
 
-  const list = createElement('ul', {
-    className: 'goods__list',
-  }, {
-    parent: container,
-    appends: listCard
-  });
+  const list = createElement(
+    'ul',
+    {
+      className: 'goods__list',
+    },
+    {
+      parent: container,
+      appends: listCard,
+    }
+  );
 
   // goods.innerHTML = `
   //   <div class="container">
