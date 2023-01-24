502c0dfe5586c96bc675bc2e27d3509992899ef5
NikSkor <mr-zerx@yandex.ru>
2023-01-24 22:07:55 +0500

    search panel added

--- src/index.js
+++ src/index.js
@@ -36,6 +36,10 @@ const init = async () => {
       menMainPage();
     });
 
+    router.on('search', (data) => {
+      console.log(data.params.value);
+    });
+
     // setTimeout(() => {
     //   router.navigate('men');
     // }, 3000)
