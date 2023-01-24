e87c60d014eeeebfa964dd98fe698d531da97a38
NikSkor <mr-zerx@yandex.ru>
2023-01-24 20:30:26 +0500

    renderFooter updated

--- src/index.js
+++ src/index.js
@@ -1,52 +1,66 @@
 import './index.html';
 import './index.scss';
 
-import {router} from "./modules/router";
-import {renderHeader} from './modules/render/renderHeader';
-import {renderFooter} from './modules/render/renderFooter';
-import {mainPage} from './modules/mainPage/mainPage';
-import {womenMainPage} from './modules/mainPage/womenMainPage';
-import {menMainPage} from './modules/mainPage/menMainPage';
-import {getData} from './modules/getData';
-import {API_URL, DATA} from './modules/const';
+import { router } from './modules/router';
+import { renderHeader } from './modules/render/renderHeader';
+import { renderFooter } from './modules/render/renderFooter';
+import { mainPage } from './modules/mainPage/mainPage';
+import { womenMainPage } from './modules/mainPage/womenMainPage';
+import { menMainPage } from './modules/mainPage/menMainPage';
+import { getData } from './modules/getData';
+import { API_URL, DATA } from './modules/const';
 import { createCssColors } from './modules/createCssColors';
+import { createElement } from './modules/createElement';
 
 const init = async () => {
-  DATA.navigation = await getData(`${API_URL}/api/categories`, {});
-  DATA.colors = await getData(`${API_URL}/api/colors`);
+  try {
+    router.on('*', () => {
+      renderHeader();
+      renderFooter();
+    });
 
-  createCssColors(DATA.colors);
-  router.on('*', () => {
-    renderHeader();
-    renderFooter();
-  });
+    DATA.navigation = await getData(`${API_URL}/api/categories`, {});
+    DATA.colors = await getData(`${API_URL}/api/colors`);
 
-  router.on('/', () => {
-    mainPage();
-  });
+    createCssColors(DATA.colors);
 
-  router.on('women', () => {
-    womenMainPage();
-  });
+    router.on('/', () => {
+      mainPage();
+    });
 
-  router.on('men', () => {
-    menMainPage();
-  });
+    router.on('women', () => {
+      womenMainPage();
+    });
 
-  // setTimeout(() => {
-  //   router.navigate('men');
-  // }, 3000)
+    router.on('men', () => {
+      menMainPage();
+    });
 
-  // setTimeout(() => {
-  //   router.navigate('women');
-  // }, 6000);
+    // setTimeout(() => {
+    //   router.navigate('men');
+    // }, 3000)
 
-  router.resolve();
+    // setTimeout(() => {
+    //   router.navigate('women');
+    // }, 6000);
 
-}
+    
+  } catch (err) {
+    createElement(
+      'h2',
+      {
+        textContent: 'Что-то пошло не так, попробуйте позже...',
+      },
+      {
+        parent: document.querySelector('.main'),
+        cb(h2) {
+          h2.style.textAlign = 'center';
+        }
+      }
+    );
+  } finally {
+    router.resolve();
+  }
+};
 
 init();
-
-
-
-
